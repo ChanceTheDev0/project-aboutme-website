1a677b4d78eafa02531e73e70a109b0022443173
Chance <111244627+TakingChances01@users.noreply.github.com>
2022-12-11 20:47:55 -0500

    add link to JS file,add functionality to theme button with JS

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementById('theme');
+const body = document.body;
+
+// Attach a click event listener to the button
+toggleButton.addEventListener('click', function() {
+  // If the body element has the "dark-theme" class, remove it and add the "light-theme" class
+  if (body.classList.contains('dark-theme')) {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+  }
+  // Otherwise, remove the "light-theme" class and add the "dark-theme" class
+  else {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+  }
+});
